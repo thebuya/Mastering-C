109614be1655c688df0badd60d099eb2dcae7e8a
thebuya <thebuya23@gmail.com>
2022-08-30 00:21:59 +0300

    Chapter_2.5

---
+++ Head-First-C/Chapter_2.5/summary.c
@@ -0,0 +1,9 @@
+			/* ------- String-Theory -------- */
+
+/*______You’ll see how to create an array of strings and then take a close look at how to search within strings using the strstr() function. In the upcomg programme, I had to create a search experience that works! */
+
+
+
+
+
+/*		*/
